d5357adb785d8526222cc88be593b088c554db6d
Amaury Balmer <amaury@beapi.fr>
2013-10-31 16:48:25 +0100

    Remove unused CSS class, add taxo column, add delete link

--- views/admin/server-page-settings.php
+++ views/admin/server-page-settings.php
@@ -8,16 +8,16 @@
 		<table class="widefat tag fixed" cellspacing="0">
 			<thead>
 				<tr>
-					<th scope="col" class="manage-column column-name"><?php _e( 'Name', BEA_CSF_LOCALE ); ?></th>
-					<th scope="col" class="manage-column column-name"><?php _e( 'Enable ?', BEA_CSF_LOCALE ); ?></th>
-					<th scope="col" class="manage-column column-name"><?php _e( 'Confict ?', BEA_CSF_LOCALE ); ?></th>
+					<th scope="col" class="manage-column "><?php _e( 'Name', BEA_CSF_LOCALE ); ?></th>
+					<th scope="col" class="manage-column "><?php _e( 'Enable ?', BEA_CSF_LOCALE ); ?></th>
+					<th scope="col" class="manage-column "><?php _e( 'Confict ?', BEA_CSF_LOCALE ); ?></th>
 					<th scope="col"><?php _e( 'Post type', BEA_CSF_LOCALE ); ?></th>
+					<th scope="col"><?php _e( 'Taxonomies', BEA_CSF_LOCALE ); ?></th>
 					<th scope="col"><?php _e( 'Mode', BEA_CSF_LOCALE ); ?></th>
 					<th scope="col"><?php _e( 'Default status', BEA_CSF_LOCALE ); ?></th>
 					<th scope="col"><?php _e( 'Notifications ?', BEA_CSF_LOCALE ); ?></th>
 					<th scope="col"><?php _e( 'Emitters', BEA_CSF_LOCALE ); ?></th>
 					<th scope="col"><?php _e( 'Receivers', BEA_CSF_LOCALE ); ?></th>
-					<th scope="col"><?php _e( 'Actions', BEA_CSF_LOCALE ); ?></th>
 				</tr>
 			</thead>
 			<tbody id="the-list" class="list:clients">
@@ -45,23 +45,30 @@
 						$class = ( $class == 'alternate' ) ? '' : 'alternate';
 						?>
 						<tr class="<?php echo $class; ?>">
-							<td class="name column-name"><strong><?php echo esc_html( $sync->get_field('label') ); ?></strong></td>
-							<td class="name column-name"><?php echo esc_html( $i18n_true_false[$sync->get_field('active')] ); ?></td>
-							<td class="name column-name"><strong><?php echo esc_html( $i18n_true_false[$sync->has_conflict()] ); ?></strong></td>
-							<td class="name column-name"><?php echo esc_html( $post_type_label ); ?></td>
-							<td class="name column-name"><?php echo esc_html( $sync->get_field('mode') ); ?></td>
-							<td class="name column-name"><?php echo esc_html( $sync->get_field('status') ); ?></td>
-							<td class="name column-name"><?php echo esc_html( $i18n_true_false[$sync->get_field('notifications')] ); ?></td>
-							<td class="name column-name"><?php echo esc_html( implode( ', ', self::get_sites($sync->get_field('emitters'), 'blogname') ) ); ?></td>
-							<td class="name column-name"><?php echo esc_html( implode( ', ', self::get_sites($sync->get_field('receivers'), 'blogname') ) ); ?></td>
 							<td>
-								<?php if ( !$sync->is_locked() ) : ?>
-								<a class="button" href="<?php echo wp_nonce_url( add_query_arg( array('action' => 'edit', 'sync_id' => $sync->get_field('id') ), network_admin_url( 'admin.php?page=' . self::admin_slug.'-add' ) ), '' ); ?>"><?php _e( 'Edit', BEA_CSF_LOCALE ); ?></a>
-									<!--<a class="delete" onclick="return confirm('<?php echo esc_js( sprintf( __( "You are about to fush clients for '%s'\n  'Cancel' to stop, 'OK' to delete." ), $sync->get_field('name') ) ); ?>');" href="<?php echo wp_nonce_url( network_admin_url( 'settings.php?page=' . self::admin_slug ) . '&amp;action=flush&amp;label=' . esc_attr( $sync->get_field('label') ), 'flush-client-' . $sync->get_field('label') ); ?>"><?php _e( 'Flush clients', BEA_CSF_LOCALE ); ?></a>-->
-								<?php else : ?>
-									<?php _e( 'This item is locked because registered since the developer API.', BEA_CSF_LOCALE ); ?>
-								<?php endif; ?>
+								<strong><?php echo esc_html( $sync->get_field('label') ); ?></strong>
+								<div class="row-actions">
+									<span class="edit">
+										<?php if ( !$sync->is_locked() ) : ?>
+											<a href="<?php echo wp_nonce_url( add_query_arg( array('action' => 'edit', 'sync_id' => $sync->get_field('id') ), network_admin_url( 'admin.php?page=' . self::admin_slug.'-add' ) ), '' ); ?>"><?php _e( 'Edit', BEA_CSF_LOCALE ); ?></a>
+											|
+											<a class="delete" href="<?php echo wp_nonce_url( add_query_arg( array('action' => 'delete', 'sync_id' => $sync->get_field('id') ), network_admin_url( 'admin.php?page=' . self::admin_slug.'-edit' ) ), '' ); ?>" onclick="return confirm('<?php echo esc_js( sprintf( __( "You are about to delete this sync '%s'\n  'Cancel' to stop, 'OK' to delete." ), $sync->get_field('label') ) ); ?>');"><?php _e( 'Delete', BEA_CSF_LOCALE ); ?></a>
+											<!--<a class="delete" onclick="return confirm('<?php echo esc_js( sprintf( __( "You are about to fush clients for '%s'\n  'Cancel' to stop, 'OK' to delete." ), $sync->get_field('name') ) ); ?>');" href="<?php echo wp_nonce_url( network_admin_url( 'settings.php?page=' . self::admin_slug ) . '&amp;action=flush&amp;label=' . esc_attr( $sync->get_field('label') ), 'flush-client-' . $sync->get_field('label') ); ?>"><?php _e( 'Flush clients', BEA_CSF_LOCALE ); ?></a>-->
+										<?php else : ?>
+											<?php _e( 'This item is locked because registered since the developer API.', BEA_CSF_LOCALE ); ?>
+										<?php endif; ?>
+									</span>
+								</div>
 							</td>
+							<td><?php echo esc_html( $i18n_true_false[$sync->get_field('active')] ); ?></td>
+							<td><strong><?php echo esc_html( $i18n_true_false[$sync->has_conflict()] ); ?></strong></td>
+							<td><?php echo esc_html( $post_type_label ); ?></td>
+							<td><?php echo esc_html( 'tAXOS' ); ?></td>
+							<td><?php echo esc_html( $sync->get_field('mode') ); ?></td>
+							<td><?php echo esc_html( $sync->get_field('status') ); ?></td>
+							<td><?php echo esc_html( $i18n_true_false[$sync->get_field('notifications')] ); ?></td>
+							<td><?php echo implode( '<br />', self::get_sites($sync->get_field('emitters'), 'blogname') ); ?></td>
+							<td><?php echo implode( '<br />', self::get_sites($sync->get_field('receivers'), 'blogname') ); ?></td>
 						</tr>
 						<?php
 					endforeach;
