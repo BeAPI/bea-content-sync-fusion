7f8bcd722a0620aab0767d0c516f6590ce3cd36e
Alexandre Sadowski <asadowski@beapi.fr>
2017-06-21 11:14:08 +0200

    use get_post_for_any ( post type )

--- classes/client/post_type.php
+++ classes/client/post_type.php
@@ -11,19 +11,17 @@ class BEA_CSF_Client_PostType {
 	 * @return mixed|null|void
 	 */
 	public static function merge( array $data, array $sync_fields ) {
-		global $wpdb;
-
 		// Clean values
 		if ( empty( $data ) || ! is_array( $data ) ) {
 			return new WP_Error( 'invalid_datas', 'Error - Datas is invalid.' );
 		}
 
 		// Post exists ?
-		$local_id = BEA_CSF_Relations::get_post_id_from_receiver( $sync_fields['_current_receiver_blog_id'], $data['blogid'], $data['ID'] );
+		$local_id = BEA_CSF_Relations::get_post_for_any( $data['blogid'], $sync_fields['_current_receiver_blog_id'], $data['ID'], $data['ID'] );
 
 		// Find local parent ?
 		if ( isset( $data['post_parent'] ) ) {
-			$local_parent_id     = BEA_CSF_Relations::get_post_id_from_receiver( $sync_fields['_current_receiver_blog_id'], $data['blogid'], $data['post_parent'] );
+			$local_parent_id     = BEA_CSF_Relations::get_post_for_any( $sync_fields['_current_receiver_blog_id'], $data['blogid'], $data['post_parent'], $data['post_parent'] );
 			$data['post_parent'] = ! empty( $local_parent_id ) && (int) $local_parent_id->emitter_id > 0 ? $local_parent_id->emitter_id : 0;
 		}
 
@@ -32,8 +30,8 @@ class BEA_CSF_Client_PostType {
 		unset( $data_for_post['medias'], $data_for_post['terms'], $data_for_post['tags_input'], $data_for_post['post_category'] );
 
 		// Merge post
-		if ( ! empty( $local_id ) && (int) $local_id->emitter_id > 0 ) {
-			$data_for_post['ID'] = $local_id->emitter_id;
+		if ( ! empty( $local_id ) && (int) $local_id > 0 ) {
+			$data_for_post['ID'] = $local_id;
 			$new_post_id         = wp_update_post( $data_for_post, true );
 
 		} else {
@@ -84,9 +82,9 @@ class BEA_CSF_Client_PostType {
 					continue;
 				}
 
-				$local_term_id = BEA_CSF_Relations::get_post_id_from_receiver( $sync_fields['_current_receiver_blog_id'], $data['blogid'], (int) $term['term_id'] );
+				$local_term_id = BEA_CSF_Relations::get_post_id_for_receiver( $sync_fields['_current_receiver_blog_id'], $data['blogid'], (int) $term['term_id'] );
 				if ( is_null( $local_term_id ) ) {
-					$local_term_id = BEA_CSF_Relations::get_post_id_from_emitter( $data['blogid'], $sync_fields['_current_receiver_blog_id'], (int) $term['term_id'] );
+					$local_term_id = BEA_CSF_Relations::get_post_id_for_emitter( $data['blogid'], $sync_fields['_current_receiver_blog_id'], (int) $term['term_id'] );
 				}
 
 				$local_t_id = 0;
@@ -117,7 +115,7 @@ class BEA_CSF_Client_PostType {
 			// Loop for medias
 			foreach ( $data['medias'] as $media ) {
 				// Media exists ?
-				$current_media_id = BEA_CSF_Relations::get_post_id_from_emitter( $data['blogid'], $sync_fields['_current_receiver_blog_id'], $media['ID'] );
+				$current_media_id = BEA_CSF_Relations::get_post_id_for_emitter( $data['blogid'], $sync_fields['_current_receiver_blog_id'], $media['ID'] );
 				if ( empty( $current_media_id ) ) {
 					//TO DO Insert new media ???
 					continue;
@@ -132,7 +130,7 @@ class BEA_CSF_Client_PostType {
 		}
 
 		// Restore post thumb
-		$thumbnail_id = BEA_CSF_Relations::get_post_id_from_emitter( $data['blogid'], $sync_fields['_current_receiver_blog_id'], $data['_thumbnail_id'] );
+		$thumbnail_id = BEA_CSF_Relations::get_post_id_for_emitter( $data['blogid'], $sync_fields['_current_receiver_blog_id'], $data['_thumbnail_id'] );
 		if ( ! empty( $thumbnail_id ) && (int) $thumbnail_id->receiver_id > 0 ) {
 			update_post_meta( $new_post_id, '_thumbnail_id', $thumbnail_id->receiver_id );
 		} elseif ( false != $data['_thumbnail'] ) {
@@ -145,7 +143,7 @@ class BEA_CSF_Client_PostType {
 
 		$new_post = get_post( $new_post_id );
 		if ( ! empty( $new_post ) ) {
-			$new_post->is_edition = ( ! empty( $local_id ) && (int) $local_id->emitter_id ) ? true : false;
+			$new_post->is_edition = ( ! empty( $local_id ) && (int) $local_id ) ? true : false;
 		}
 
 		return apply_filters( 'bea_csf.client.posttype.merge', $data, $sync_fields, $new_post );
