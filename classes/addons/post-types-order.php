4f5339b92913930e9895dd9134bb23d464fa653b
Maxime Culea <culea.maxime@gmail.com>
2017-10-16 22:21:38 +0200

    #10 : Check if the post's menu_order changed to launch wp_update_post

--- classes/addons/post-types-order.php
+++ classes/addons/post-types-order.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Class BEA_CSF_Addon_Post_Types_Order
  *
@@ -15,18 +16,31 @@ class BEA_CSF_Addon_Post_Types_Order {
 	/**
 	 * On ajax menu_order update, prefer update the post for CSF to be hooked
 	 *
-	 * @param $data
+	 * @param array $data : only contains the menu_order
 	 * @param $key
-	 * @param $id
+	 * @param int $post_id : the post_id
 	 *
 	 * @author Maxime CULEA
 	 *
 	 * @return int
 	 */
-	public function wp_update_post_order( $data, $key, $id ) {
-		// $data only contains the menu_order
-		wp_update_post( wp_parse_args( $data, [ 'ID' => $id ] ) );
-		// Return 0 for not updating twice the same data
+	public function wp_update_post_order( $data, $key, $post_id ) {
+		// Check $data constancy
+		if ( empty( $data ) || ! isset( $data['menu_order'] ) ) {
+			return $data;
+		}
+
+		// Get post's menu order to compare with the given one
+		$menu_order = get_post_field( 'menu_order', $post_id );
+		if ( empty( $menu_order ) || $menu_order == $data['menu_order'] ) {
+			// If same, early return
+			return $data;
+		}
+
+		// Update the post with wp_update_post to allow CSF to hook on
+		wp_update_post( wp_parse_args( $data, [ 'ID' => $post_id ] ) );
+
+		// Return 0 for not updating twice the same post
 		return 0;
 	}
 }
