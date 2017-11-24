e70036703fbf417c0af51b7b21fd434733f440b6
Amaury BALMER <amaury@beapi.fr>
2017-11-24 08:24:08 +0100

    add local/remote selector on media library

--- classes/admin/admin-blog.php
+++ classes/admin/admin-blog.php
@@ -15,6 +15,74 @@ class BEA_CSF_Admin_Blog {
 
 		// Add widget on each site dashboard
 		add_action( 'wp_dashboard_setup', array(__CLASS__, 'wp_dashboard_setup') );
+
+		// Allow filter media view by origin
+		add_filter( 'restrict_manage_posts', array(__CLASS__, 'restrict_manage_posts'), 10, 2 );
+		add_filter( 'posts_join', array(__CLASS__, 'posts_join'), 10, 2 );
+		add_filter( 'posts_where', array(__CLASS__, 'posts_where'), 10, 2 );
+	}
+
+	/**
+	 * Add a selector for filter local or remote media
+	 *
+	 * @param $post_type
+	 * @param $which
+	 *
+	 * @return bool
+	 */
+	public static function restrict_manage_posts( $post_type, $which ) {
+		if ( $post_type !== 'attachment' || $which != 'bar' ) {
+			return false;
+		}
+
+		$current_action = ( !isset($_GET['attachment-bea-csf-filter']) ) ? '' : $_GET['attachment-bea-csf-filter'];
+
+		$output = '<label for="attachment-filter" class="screen-reader-text">'.__('Show all media', 'bea-content-sync-fusion').'</label>';
+		$output .= '<select class="attachment-filters" name="attachment-bea-csf-filter" id="attachment-bea-csf-filter">';
+		$output .= '<option value="">'.__('Show all media', 'bea-content-sync-fusion').'</option>';
+		$output .= '<option '.selected($current_action, 'local-only', false).' value="local-only">'.__('Show only local media', 'bea-content-sync-fusion').'</option>';
+		$output .= '<option '.selected($current_action, 'remote-only', false).' value="remote-only">'.__('Show only remote media', 'bea-content-sync-fusion').'</option>';
+		$output .= '</select>';
+
+		echo $output;
+	}
+
+	/**
+	 * Add join with relations tables for filter local or remote media
+	 *
+	 * @param $join
+	 * @param WP_Query $query
+	 *
+	 * @return string
+	 */
+	public static function posts_join( $join, WP_Query $query ) {
+		global $wpdb;
+
+		if ( !isset($_GET['attachment-bea-csf-filter']) || !$query->is_main_query() || empty($_GET['attachment-bea-csf-filter']) ) {
+			return $join;
+		}
+
+		$join_type = $_GET['attachment-bea-csf-filter'] == 'local-only' ? 'LEFT' : 'INNER';
+
+		$join .= " $join_type JOIN $wpdb->bea_csf_relations AS bcr ON ( $wpdb->posts.ID = bcr.receiver_id AND bcr.receiver_blog_id = " . get_current_blog_id() . " ) ";
+		return $join;
+	}
+
+	/**
+	 * Add join with relations tables for filter local
+	 *
+	 * @param $where
+	 * @param WP_Query $query
+	 *
+	 * @return string
+	 */
+	public static function posts_where( $where, WP_Query $query ) {
+		if ( !isset($_GET['attachment-bea-csf-filter']) || !$query->is_main_query() || $_GET['attachment-bea-csf-filter'] != 'local-only' ) {
+			return $where;
+		}
+
+		$where .= " AND bcr.receiver_id IS NULL ";
+		return $where;
 	}
 
 	/**
